b4a2c312a7a3b2599cc07c99653fd5413123496f
Kim <futureofyouandme@gmail.com>
2017-09-04 12:39:49 +0900

    hangul -> eng

--- Project1/Project1/b.cpp
+++ Project1/Project1/b.cpp
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-
-void º¯¿µÃ¶() {
+void byun() {
 
 }
 
 void main() {
 	printf("Hello,World!\n");
 	getchar();
-<<<<<<< HEAD
-}
-=======
 }
 
->>>>>>> 1464e966986a38ffcfee024a0540b1719fdb8139
